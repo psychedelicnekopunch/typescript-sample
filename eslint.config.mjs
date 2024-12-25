eaeb0d043b17ef3fa9fd5dceb373d34389269c59
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-12-25 15:14:43 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from "eslint-config-prettier"
 
 
 // export default tseslint.config(
